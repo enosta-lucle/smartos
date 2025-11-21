b567515551090109a5509211a7985bb9ded2cd5f
luc.le <luc.le@enosta.team.com>
2025-11-21 11:38:30 +0700

    feat(strapi): add dump content type with controller, service and routes
    Add new dump collection type with button component support and populate-all plugin
    Update pnpm-workspace.yaml with additional build dependencies

---
+++ apps/strapi/src/api/dump/controllers/dump.ts
@@ -0,0 +1,7 @@
+/**
+ * dump controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::dump.dump');
---
+++ apps/strapi/src/api/dump/routes/dump.ts
@@ -0,0 +1,7 @@
+/**
+ * dump router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::dump.dump');
