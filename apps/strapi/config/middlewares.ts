6d290ffbd1d6c2fc317dc0b254c410424343780b
luc.le <luc.le@enosta.team.com>
2025-11-27 11:43:07 +0700

    feat(strapi): add AWS S3 upload provider and security config
    - Add @strapi/provider-upload-aws-s3 dependency
    - Configure AWS S3 upload provider with environment variables
    - Update security middleware to allow S3 bucket resources
    - Format JSON and TS files consistently

--- apps/strapi/config/middlewares.ts
+++ apps/strapi/config/middlewares.ts
@@ -1,12 +1,36 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+	"strapi::logger",
+	"strapi::errors",
+	{
+		name: "strapi::security",
+		config: {
+			contentSecurityPolicy: {
+				useDefaults: true,
+				directives: {
+					"connect-src": ["'self'", "https:"],
+					"img-src": [
+						"'self'",
+						"data:",
+						"blob:",
+						"smartos-landing-page.s3.ap-southeast-1.amazonaws.com",
+					],
+					"media-src": [
+						"'self'",
+						"data:",
+						"blob:",
+						"dl.airtable.com",
+						"smartos-landing-page.s3.ap-southeast-1.amazonaws.com",
+					],
+					upgradeInsecureRequests: null,
+				},
+			},
+		},
+	},
+	"strapi::cors",
+	"strapi::poweredBy",
+	"strapi::query",
+	"strapi::body",
+	"strapi::session",
+	"strapi::favicon",
+	"strapi::public",
 ];
--- apps/strapi/config/plugins.ts
+++ apps/strapi/config/plugins.ts
@@ -1,4 +1,4 @@
-export default () => ({
+export default ({ env }) => ({
 	seo: { enabled: true },
 	"icons-field": {
 		enabled: true,
@@ -8,5 +8,29 @@ export default () => ({
 	},
 	publisher: {
 		enabled: true,
+	},
+	upload: {
+		config: {
+			provider: "aws-s3",
+			providerOptions: {
+				s3Options: {
+					credentials: {
+						accessKeyId: env("AWS_ACCESS_KEY_ID"),
+						secretAccessKey: env("AWS_ACCESS_SECRET"),
+					},
+					region: env("AWS_REGION"),
+					params: {
+						ACL: env("AWS_ACL", "public-read"),
+						signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
+						Bucket: env("AWS_BUCKET_NAME"),
+					},
+				},
+			},
+			actionOptions: {
+				upload: {},
+				uploadStream: {},
+				delete: {},
+			},
+		},
 	},
 });
